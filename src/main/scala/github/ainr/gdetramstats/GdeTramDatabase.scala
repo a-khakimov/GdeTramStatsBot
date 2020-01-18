d6f4bd50a28819ca3e0060dab739e11469069222
a-khakimov <techlinked@yandex.ru>
2020-01-19 00:59:20 +0500

    implement some commands

--- src/main/scala/github/ainr/gdetramstats/GdeTramDatabase.scala
+++ src/main/scala/github/ainr/gdetramstats/GdeTramDatabase.scala
@@ -6,6 +6,7 @@ import doobie.free.connection.ConnectionIO
 import doobie.implicits._
 import doobie.util.ExecutionContexts
 import doobie.util.transactor.Transactor
+import shapeless.PolyDefns.->
 
 
 class GdeTramDatabase {
@@ -17,24 +18,50 @@ class GdeTramDatabase {
   val pass = conf.getString("GdeTramStats.doobie.password")
 
   implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
-
   val transactor = Transactor.fromDriverManager[IO](driver, url, user, pass)
 
-  def maxStops(): String = {
-    val program: ConnectionIO[List[String]] = sql"select name from country".query[String].to[List]
-    val task: IO[List[String]] = program.transact(transactor)
-    val result: List[String] = task.unsafeRunSync
-    result.toString
+  def uniqueVkUsersWeek(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(distinct user_id) from journal where (time::date < (current_date) and time::date >= (current_date - INTERVAL '7 day') and platform='vk')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
   }
 
-  /*
-    val program: ConnectionIO[String] = sql"select name from country where code='96'".query[String].unique
-    val task: IO[String] = program.transact(transactor)
-    val result: String = task.unsafeRunSync
-    println(s"Result ${result}")
-  */
+  def uniqueVkUsersDay(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(distinct user_id) from journal where (time::date = (current_date - INTERVAL '1 day') and platform='vk')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
+  }
 
-  /*
+  def uniqueTgUsersWeek(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(distinct user_id) from journal where (time::date < (current_date) and time::date >= (current_date - INTERVAL '7 day') and platform='tg')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
+  }
+
+  def uniqueTgUsersDay(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(distinct user_id) from journal where (time::date = (current_date - INTERVAL '1 day') and platform='tg')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
+  }
+
+
+  def numberVkMessagesWeek(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(user_id) from journal where (time::date < (current_date) and time::date >= (current_date - INTERVAL '7 day') and platform='vk')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
+  }
+
+  def numberVkMessagesDay(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(user_id) from journal where (time::date = (current_date - INTERVAL '1 day') and platform='vk')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
+  }
+
+  def numberTgMessagesWeek(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(user_id) from journal where (time::date < (current_date) and time::date >= (current_date - INTERVAL '7 day') and platform='tg')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
+  }
+
+  def numberTgMessagesDay(): Int = {
+    val program: ConnectionIO[Int] = sql"select count(user_id) from journal where (time::date = (current_date - INTERVAL '1 day') and platform='tg')".query[Int].unique
+    program.transact(transactor).unsafeRunSync()
+  }
+
+  /* For example
     val query : String = "country"
     val program: ConnectionIO[List[String]] = sql"select name from country".query[String].to[List]
     val task: IO[List[String]] = program.transact(transactor)
--- src/main/scala/github/ainr/gdetramstats/GdeTramStatsTgBot.scala
+++ src/main/scala/github/ainr/gdetramstats/GdeTramStatsTgBot.scala
@@ -17,20 +17,38 @@ class GdeTramStatsTgBot(token: String) extends TgBot(token)
   val gdetramdb = new GdeTramDatabase
   override val client = new ScalajHttpClient(token)
 
-  onCommand("max" | "roll") { implicit msg =>
-    val r = gdetramdb.maxStops()
-    reply(r).void
-  }
+  onCommand("uniqueVkUsersDay")  { implicit msg => reply(gdetramdb.uniqueVkUsersDay().toString).void }
+  onCommand("uniqueVkUsersWeek") { implicit msg => reply(gdetramdb.uniqueVkUsersWeek().toString).void }
+  onCommand("uniqueTgUsersDay")  { implicit msg => reply(gdetramdb.uniqueTgUsersDay().toString).void }
+  onCommand("uniqueVkUsersWeek") { implicit msg => reply(gdetramdb.uniqueTgUsersWeek().toString).void }
+
+  onCommand("numberVkMessagesDay")  { implicit msg => reply(gdetramdb.numberVkMessagesDay().toString).void }
+  onCommand("numberVkMessagesWeek") { implicit msg => reply(gdetramdb.numberVkMessagesWeek().toString).void }
+  onCommand("numberTgMessagesDay")  { implicit msg => reply(gdetramdb.numberTgMessagesDay().toString).void }
+  onCommand("numberTgMessagesWeek") { implicit msg => reply(gdetramdb.numberTgMessagesWeek().toString).void }
 
   onCommand("help") { implicit msg =>
     val help = "" +
-      "/help" +
-      "/max - description"
+      "/help\n" +
+      "\n" +
+      "/uniqueVkUsersDay - Количество уникальных пользователей в вк за сутки\n" +
+      "/uniqueVkUsersWeek - Количество уникальных пользователей в вк за неделю\n" +
+      "/uniqueTgUsersDay - Количество уникальных пользователей в телеграм за сутки\n" +
+      "/uniqueVkUsersWeek - Количество уникальных пользователей в телеграм за неделю\n" +
+      "\n" +
+      "/numberVkMessagesDay - Общее количество сообщений в вк за сутки\n" +
+      "/numberVkMessagesWeek - Общее количество сообщений в вк за неделю\n" +
+      "/numberTgMessagesDay - Общее количество сообщений в телеграм за сутки\n" +
+      "/numberTgMessagesWeek - Общее количество сообщений в телеграм за неделю\n" +
+      "\n" +
+      ""
     reply(help).void
   }
-
+/*
   override def receiveMessage(msg: Message): Future[Unit] =
     msg.text.fold(Future.successful(())) { text =>
       request(SendMessage(msg.source, "")).void
     }
+
+ */
 }
